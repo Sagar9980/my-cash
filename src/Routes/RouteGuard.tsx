8eb6804f3f18f049330f8cee45e893b2cb5b42c5
Khem Singh Dhami <sagardhami2001@gmail.com>
2022-12-11 22:12:35 +0545

    depricated js-cookie

--- src/Routes/RouteGuard.tsx
+++ src/Routes/RouteGuard.tsx
@@ -1,9 +1,10 @@
-import { Outlet, Navigate } from 'react-router';
-import Cookies from 'js-cookie';
+import { Outlet, Navigate } from "react-router";
+// import Cookies from 'js-cookie';
 
 const RouteGuard = () => {
-  const auth = Cookies.get('token');
-  return auth ? <Outlet /> : <Navigate to='auth/login' />;
+  // const auth = Cookies.get('token');
+  const auth = false;
+  return auth ? <Outlet /> : <Navigate to="auth/login" />;
 };
 
 export default RouteGuard;
