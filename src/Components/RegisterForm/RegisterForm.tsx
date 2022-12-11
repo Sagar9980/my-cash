60a1fc847a9fda8268cbe20091b39b335ff923fe
Khem Singh Dhami <sagardhami2001@gmail.com>
2022-12-11 22:28:20 +0545

    test-03

--- src/Components/RegisterForm/RegisterForm.tsx
+++ src/Components/RegisterForm/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Typography, Button, Form, Input, Row, Col, Checkbox } from "antd";
 import { ReactComponent as GoogleIcon } from "Assets/Icons/Google.svg";
 import "./RegisterForm.scss";
@@ -10,8 +10,8 @@ import { useNavigate } from "react-router-dom";
 function RegisterForm() {
   const navigate = useNavigate();
   // const dispatch = useAppDispatch();
-  const [error, setError] = useState<any>();
-  const [loading, setLoading] = useState<boolean>(false);
+  // const [error, setError] = useState<any>();
+  // const [loading, setLoading] = useState<boolean>(false);
   const [form] = useForm();
   // const values = useAppSelector((store) => store.RegisterUserReducer);
   // useEffect(() => {
@@ -42,8 +42,8 @@ function RegisterForm() {
         <Typography.Paragraph type="secondary" style={{ textAlign: "center" }}>
           Or
         </Typography.Paragraph>
-        {error && (
-          <Typography.Paragraph type="warning">{error}</Typography.Paragraph>
+        {true && (
+          <Typography.Paragraph type="warning">good</Typography.Paragraph>
         )}
         <Form layout="vertical" form={form} onFinish={handleSubmit}>
           <Form.Item
@@ -78,7 +78,7 @@ function RegisterForm() {
             </Typography.Link>
           </div>
           <Form.Item>
-            <Button type="primary" htmlType="submit" loading={loading}>
+            <Button type="primary" htmlType="submit" loading={false}>
               Register
             </Button>
           </Form.Item>
